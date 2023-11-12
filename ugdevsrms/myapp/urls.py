101298923136f2346a468c6652bbb6b686678e1b
kyarminrox <51434042+kyarminrox@users.noreply.github.com>
2023-11-11 22:50:07 -0800

    Updated Bootstrap version in template
    In this commit, the Bootstrap version used in the template 'mytemplate.html' was updated. This change ensures that the latest features, improvements, and bug fixes from the Bootstrap library are available for use in our project. The 'static' tag was also confirmed to be correctly loaded, ensuring proper handling of static files like CSS. The update contributes to maintaining the modern look and feel of the application and enhances the overall user experience.

---
+++ ugdevsrms/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),  # replace 'myapp' with the name of your app
+]
+# Path: ugdevsrms/myapp/urls.py
+
+urlpatterns = [
+    path('', views.my_view, name='my_view'),
+]   
--- ugdevsrms/ugdevsrms/settings.py
+++ ugdevsrms/ugdevsrms/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'myapp',
+    'ugdevsrms',
 ]
 
 MIDDLEWARE = [
